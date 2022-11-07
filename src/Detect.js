9327295b221371d340ff72edc915f5c4ab6fc0d5
krishgupta-x <krish.gupta@xerox.com>
2022-11-07 10:59:19 -0800

    FPS display fix

--- src/Detect.js
+++ src/Detect.js
@@ -193,7 +193,7 @@ function App() {
             const api = await predictions(screen);
             if(first){
                 t1 = performance.now();
-                setFPS("Load Time: " +(t1 - t0) + " milliseconds");
+                setFPS("Load Time: " +(t1 - t0).toFixed(3) + " milliseconds");
                 first = false;
             }
             if(api != "error") boxes.current = api;
